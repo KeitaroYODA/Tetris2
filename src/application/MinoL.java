e28947eb086aa9c11ecd2cfb3e578aedf9a450a2
YODA <YODA@192.168.1.7>
2018-06-26 17:37:05 +0900

    ミノＬ字追加

---
+++ src/application/MinoL.java
@@ -0,0 +1,49 @@
+package application;
+
+
+class MinoL extends Mino {
+	public MinoL() {
+		super();
+
+		// ミノを構成するパネルの位置を指定（行、列）
+		this.panelArray[1][2] = 1;
+		this.panelArray[2][0] = 1;
+		this.panelArray[2][1] = 1;
+		this.panelArray[2][2] = 1;
+
+		this.panel = new Panel(TetrisImage.minoL);
+	}
+
+	// ミノを回転
+	public void turn() {
+		// パネルの配列情報を初期化
+		this.initPanel();
+
+		switch(this.direction) {
+		case DIRECTION_NORMAL: // 正面
+			this.panelArray[1][2] = 1;
+			this.panelArray[2][0] = 1;
+			this.panelArray[2][1] = 1;
+			this.panelArray[2][2] = 1;
+			break;
+		case DIRECTION_RIGHT: // 右向き
+			this.panelArray[1][1] = 1;
+			this.panelArray[2][1] = 1;
+			this.panelArray[3][1] = 1;
+			this.panelArray[3][2] = 1;
+			break;
+		case DIRECTION_REVERCE: // 上下逆
+			this.panelArray[2][0] = 1;
+			this.panelArray[2][1] = 1;
+			this.panelArray[2][2] = 1;
+			this.panelArray[3][0] = 1;
+			break;
+		case DIRECTION_LEFT: // 左向き
+			this.panelArray[1][0] = 1;
+			this.panelArray[1][1] = 1;
+			this.panelArray[2][1] = 1;
+			this.panelArray[3][1] = 1;
+			break;
+		}
+	}
+}
